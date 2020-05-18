4425fcdd113b2a9f4b1e8e0e5960f8aef0a4f0f6
Kevin Boston <kbos22@gmail.com>
2020-05-18 20:24:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,30 +9,37 @@ class Song
   def save
     self.class.all << self
   end
+
   def self.create
-    song.new 
+    song = Song.new
     song.save
     song
   end
-  def self.new_by_name(s_name)
-    song.new 
-    song.name = s_name 
-    song 
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
   end
-  def self.create_by_name(s_name)
+
+  def self.create_by_name(song_name)
     song = self.create
-    song.name = s_name
-    song 
+    song.name = song_name
+    song
   end
-  def self.find_by_name(s_name)
-    self.all.detect{|s| s.name == s_name}
+
+  def self.find_by_name(song_name)
+    self.all.detect{|s| s.name == song_name}
   end
-  self.find_or_create_by_name(s_name)
-    self.find_by_name(s_name) || self.create_by_name(s_name)
+
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
   end
+
   def self.alphabetical
-    @@all.sort_by {|s| s.name}
+    self.all.sort_by{|s| s.name}
   end
+
   def self.new_from_filename(filename)
     parts = filename.split(" - ")
     artist_name = parts[0]
@@ -58,5 +65,5 @@ class Song
   def self.destroy_all
     self.all.clear
   end
-end
+
 end
